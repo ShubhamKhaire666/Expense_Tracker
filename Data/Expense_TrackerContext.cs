eeca9f0172f1a4d46b44dccae1c40597eefc7845
ShubhamKhaire666 <shubhamkhaire.edu@gmail.com>
2023-06-28 01:32:43 +0300

    Category Controller

---
+++ Data/Expense_TrackerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Expense_Tracker.Models;
+
+namespace Expense_Tracker.Data
+{
+    public class Expense_TrackerContext : DbContext
+    {
+        public Expense_TrackerContext (DbContextOptions<Expense_TrackerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Expense_Tracker.Models.Category> Category { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Expense_Tracker.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Expense_TrackerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Expense_TrackerContext") ?? throw new InvalidOperationException("Connection string 'Expense_TrackerContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
