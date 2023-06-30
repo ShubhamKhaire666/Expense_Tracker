559c3240c4dd33868cc34e3eaee055ce39c10634
ShubhamKhaire666 <shubhamkhaire.edu@gmail.com>
2023-07-01 02:06:04 +0300

    Transaction Controller Created

---
+++ Controllers/TransactionsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Expense_Tracker.Data;
+using Expense_Tracker.Models;
+
+namespace Expense_Tracker.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private readonly Expense_TrackerContext _context;
+
+        public TransactionsController(Expense_TrackerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transactions
+        public async Task<IActionResult> Index()
+        {
+            var expense_TrackerContext = _context.Transactions.Include(t => t.Category);
+            return View(await expense_TrackerContext.ToListAsync());
+        }
+
+        // GET: Transactions/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "CategoryId");
+            return View();
+        }
+
+        // POST: Transactions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TransactionId,CategoryId,Amount,Note,Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "CategoryId", transaction.CategoryId);
+            return View(transaction);
+        }
+
+        // POST: Transactions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Transactions == null)
+            {
+                return Problem("Entity set 'Expense_TrackerContext.Transactions'  is null.");
+            }
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Transactions.Remove(transaction);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
