9c7bf6aa990f7f8f3fdd452bf02410a7bd2dfeed
ShubhamKhaire666 <shubhamkhaire.edu@gmail.com>
2023-07-03 00:09:17 +0300

    Dashboard Controller

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -4,19 +4,40 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Expense_Tracker.Models
 {
-    public class Transaction
-    {
+	public class Transaction
+	{
 
-        [Key]
-        public int TransactionId { get; set; }
+		[Key]
+		public int TransactionId { get; set; }
 
-        //Category Id
-        public int CategoryId { get; set; }
-        public Category? Category { get; set; }
-        public int Amount { get; set; }
+		//Category Id
+		[Range(1,int.MaxValue,ErrorMessage ="Please select a category")]
+		public int CategoryId { get; set; }
+		public Category? Category { get; set; }
+		[Range(1,int.MaxValue,ErrorMessage ="Amount should be greater than zero.")]
+		public int Amount { get; set; }
 
-        [Column(TypeName = "nvarchar(75)")]
-        public string? Note { get; set; }
-        public DateTime Date { get; set; } = DateTime.Now;
-    }
+		[Column(TypeName = "nvarchar(75)")]
+		public string? Note { get; set; }
+		public DateTime Date { get; set; } = DateTime.Now;
+
+		[NotMapped]
+		public string? CategoryTitleWithIcon
+		{
+			get
+			{
+				return Category == null ? "" : Category.Icon + " " + Category.Title;
+			}
+		}
+
+		[NotMapped]
+		public string? FormattedAmount
+		{
+			get
+			{
+				return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0");
+			}
+
+		}
+	}
 }
